0d09f0f30abd61e73e7591aa7c19793ab738ea26
bendaojiu <bendaojiu@163.com>
2015-11-16 13:23:36 +0800

    替换字符串完善了，可以替换不同长度的字符串了，但是有一个神奇的现象出现了，strlen和sizeof用于字符串显示的数量不是差1.。。。。

--- demo02.c
+++ demo02.c
@@ -8,12 +8,10 @@
 2）	把字符串替换成（dcba11111dcba2222dcbaqqqqq），并把结果传出。
 */
 
-#define str1 "abcd"
-#define str2 "dcba"
-int strfreplace(char *inbuf, int *times, char **outbuf)
+int strfreplace(char *inbuf, char *str1, char *str2, int *times, char **outbuf)
 {
 	int ret = 0, i = 0, len = 0;
-	char *in = NULL, *out = NULL, *tmp1 = NULL, *tmp2 = NULL;
+	char *inf = NULL, *out = NULL, *tmpTostart = NULL, *tmpToend = NULL, *outf = NULL, *in = NULL;
 	if (inbuf == NULL)
 	{
 		ret = -1;
@@ -32,60 +30,69 @@ int strfreplace(char *inbuf, int *times, char **outbuf)
 		printf("接收替换后的字符串地址outbuf不能为空\n");
 		return ret;
 	}
+	tmpTostart = inbuf;
+	tmpToend = inbuf;
 	len = strlen(inbuf);
-	in = (char *)malloc((len+1)*sizeof(char));
-	memset(in, 0, (len+1));
-	out = (char *)malloc((len+1)*sizeof(char));
-	memset(out, 0, (len+1));
-	if (in == NULL)
-	{
-		ret = -4;
-		printf("内部错误。。。in分配内存出错\n");
-		return ret;
-	}
+	inf = inbuf;
+	
+	out = (char *)malloc(1024*sizeof(char));
+	memset(out, 0, 1024);
+	outf = out;
+	in = inbuf;
+	
 	if (out == NULL)
 	{
 		ret = -4;
 		printf("内部错误。。。out分配内存出错\n");
 		return ret;
 	}
-	memcpy(in, inbuf, len);
-	memcpy(out, inbuf, len);
-	tmp1 = in;
-	while(strlen(tmp1)>(strlen(str1)-1))
+	
+
+	while(tmpToend != NULL)
 	{
-		tmp2 = strstr(tmp1, str1);
-		if (tmp2 == NULL)
+		tmpTostart = strstr(inf, str1);
+		if (tmpTostart == NULL)
 		{
-			if (tmp1 == in)
+			if (tmpToend == in)
 			{
 				ret = 1;
 				break;
 			}
 			else
 			{
+				
+				memcpy(outf, tmpToend, sizeof(tmpToend));
+				*(outf + sizeof(tmpToend)) = '\0';
 				break;
 			}
 		}
 		i ++;
-		memcpy(out+(tmp2-in), str2, strlen(str2));
+	
+		memcpy(outf, tmpToend, tmpTostart - inf);
+		outf = outf + (int)(tmpTostart - inf);
+		memcpy(outf, str2, strlen(str2));
+		outf = outf + strlen(str2);
+		tmpToend = tmpTostart + strlen(str1);
+		inf = tmpToend;
 		
-		tmp1 = tmp2 + strlen(str1);
+		
 	}
-	memcpy(*outbuf, out, (len+1));
+	memcpy(*outbuf, out, strlen(out));
+	//printf("%d   %d", sizeof(out), strlen(out)); //为什么显示的不一样！！！！
+	*(*outbuf + strlen(out)) = 0;
 	*times = i;
-	free(in);
+
 	free(out);
 	return ret;
 }
 void main()
 {
 	int ret = 0, times = 0;
-	char *inbuf = "abcd11111abcd2222abcdqqqqq";
+	char *inbuf = "abcd11111abcd2222abcdqqqqq", *str1 = "abcd", *str2 = "zxcvbnm";
 	char *outbuf = NULL;
-	outbuf = (char *)malloc((strlen(inbuf)+1)*sizeof(char));
+	outbuf = (char *)malloc(1024*sizeof(char));
 	memset(outbuf, 0, (strlen(inbuf)+1));
-	ret = strfreplace(inbuf, &times, &outbuf);
+	ret = strfreplace(inbuf, str1, str2, &times, &outbuf);
 	if (ret == 0)
 	{
 		printf("共出现%d次\n", times);
